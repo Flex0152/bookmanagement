3920ca2fa5b11f503e48696fd70ff9d9e7c9b60d
Felix <flex89@web.de>
2025-04-18 11:42:30 +0200

    tkinter start

---
+++ bookmanagement/app.py
@@ -0,0 +1,33 @@
+from model import DatabaseAPI
+import tkinter as tk
+from tkinter import TclError, messagebox
+from tkinter.ttk import Label, Entry, Button, Spinbox
+
+
+class BookManagementApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Bücherverwaltung")
+
+        # Hauptcontainer 
+        self.frame = tk.Frame(self)
+        self.frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+    
+        # Frame zur Kontrolle der Button
+        self.btn_frame = tk.Frame(self)
+        self.btn_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+
+        # Hauptfenster Config
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        # vier Zeilen, vier Zeilen Rest 
+        for i in range(4):
+            self.frame.rowconfigure(i, weight=1)
+            self.frame.columnconfigure(i, weight=1)
+        
+        # zwei Zeilen; zwei Spalten Buttons
+        for i in range(2):
+            self.btn_frame.rowconfigure(i, weight=1)
+            self.btn_frame.columnconfigure(i, weight=1)
